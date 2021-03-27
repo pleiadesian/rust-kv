df5c2935f6147fbf7ef6ab4140e927d993b7cec9
Zulai Wang <wangzl31@outlook.com>
2021-03-27 23:19:25 +0800

    WIP: Proj1: Make the tests compile

---
+++ project-1/src/kv.rs
@@ -0,0 +1,21 @@
+pub struct KvStore {
+    
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        panic!("Not implemented");
+    }
+
+    pub fn set(&mut self) {
+        panic!("Not implemented");
+    }
+
+    pub fn get(&self) {
+        panic!("Not implemented");
+    }
+
+    pub fn remove(&mut self) {
+        panic!("Not implemented");
+    }
+}
