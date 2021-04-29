8c49ce6ea1423473c0627a4a868bac265d05923b
Zulai Wang <wangzl31@outlook.com>
2021-04-29 13:28:40 +0800

    Proj1: Store values in memory

--- project-1/src/kv.rs
+++ project-1/src/kv.rs
@@ -1,21 +1,25 @@
+use std::collections::HashMap;
+
 pub struct KvStore {
-    
+    kv: HashMap<String, String>,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        panic!("Not implemented");
+        KvStore {
+            kv: HashMap::new(),
+        }
     }
 
     pub fn set(&mut self, key: String, val: String) {
-        panic!("Not implemented");
+        self.kv.insert(key, val);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        panic!("Not implemented");
+        self.kv.get(&key).cloned()
     }
 
     pub fn remove(&mut self, key: String) {
-        panic!("Not implemented");
+        self.kv.remove(&key);
     }
 }
